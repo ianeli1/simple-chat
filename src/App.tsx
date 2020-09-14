2c8bafffdd5f819f159001dba310ab79eb31901a
Ian E <tevitevi0@gmail.com>
2020-09-14 15:19:46 -0600

    Added a toolbar header. Categorized some CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Box } from "@material-ui/core";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+} from "@material-ui/core";
+import { Chat, HorizontalSplit } from "@material-ui/icons";
 import "./App.css";
 import ChatBox from "./chatElements";
 import LeftSidebar from "./leftSidebar";
@@ -20,12 +27,15 @@ type AppProps = {};
 type AppState = {
   loading: boolean;
   currentChannel: string;
+  currentServer: string;
   user: null | r.User; //implement,
   channel: r.Channel;
   members: {
     [key: string]: r.User;
   };
   data: r.Server | null;
+  showRight: boolean;
+  showLeft: boolean;
 };
 class App extends React.Component<AppProps, AppState> {
   private handler: Handler;
@@ -33,11 +43,14 @@ class App extends React.Component<AppProps, AppState> {
     super(props);
     this.state = {
       loading: true,
-      currentChannel: "exampleChannel",
+      currentChannel: "",
+      currentServer: "",
       user: null,
       channel: {},
       members: {},
       data: null,
+      showRight: true,
+      showLeft: true,
     };
     this.handler = new Handler();
     this.handleChannelChange = this.handleChannelChange.bind(this);
@@ -54,7 +67,7 @@ class App extends React.Component<AppProps, AppState> {
       this.handler.loadServer(
         newServer,
         (members) => this.setState({ members }),
-        (data) => this.setState({ data })
+        (data) => this.setState({ data, currentServer: newServer })
       );
     };
   }
@@ -63,7 +76,7 @@ class App extends React.Component<AppProps, AppState> {
     return () => {
       if (newChannel !== this.state.currentChannel)
         this.handler.getChannel(newChannel, (channel) =>
-          this.setState({ channel })
+          this.setState({ channel, currentChannel: newChannel })
         );
     };
   }
@@ -71,7 +84,7 @@ class App extends React.Component<AppProps, AppState> {
   render() {
     //this looks hacky, fix
     return (
-      <Box className="App">
+      <Box className="Global">
         {!this.state.user && (
           <Login
             signIn={this.handler.signIn}
@@ -79,38 +92,89 @@ class App extends React.Component<AppProps, AppState> {
           />
         )}
         {this.state.user && (
-          <LeftSidebar
-            createServer={this.handler.createServer}
-            channelList={(this.state.data && this.state.data.channels) || []}
+          <AppToolbar
+            currentServer={this.state.currentServer}
             currentChannel={this.state.currentChannel}
-            changeServer={this.handleServerChange}
-            changeChannel={this.handleChannelChange}
-            user={this.state.user}
-            createChannel={this.handler.createChannel}
+            toggleLeft={() => this.setState({ showLeft: !this.state.showLeft })}
+            toggleRight={() =>
+              this.setState({ showRight: !this.state.showRight })
+            }
           />
         )}
-        {this.state.user && (
-          <ChatBox
-            joinServer={this.handler.joinServer}
-            user={this.state.user}
-            emotes={(this.state.data && this.state.data.emotes) || {}}
-            channel={this.state.channel}
-            sendMessage={this.handler.sendMessage}
-          />
-        )}
-        {this.state.user && (
-          <RightSidebar
-            user={this.state.user}
-            members={this.state.members}
-            emotes={(this.state.data && this.state.data.emotes) || {}}
-            addEmote={this.handler.addEmote}
-            signOut={this.handler.signOut}
-            debug={this.handler.getDebug}
-          />
-        )}
+        <Box className="App">
+          {this.state.user && this.state.showLeft && (
+            <LeftSidebar
+              createServer={this.handler.createServer}
+              channelList={(this.state.data && this.state.data.channels) || []}
+              currentChannel={this.state.currentChannel}
+              changeServer={this.handleServerChange}
+              changeChannel={this.handleChannelChange}
+              user={this.state.user}
+              createChannel={this.handler.createChannel}
+            />
+          )}
+          {this.state.user && (
+            <ChatBox
+              joinServer={this.handler.joinServer}
+              user={this.state.user}
+              emotes={(this.state.data && this.state.data.emotes) || {}}
+              channel={this.state.channel}
+              sendMessage={this.handler.sendMessage}
+            />
+          )}
+          {this.state.user && this.state.showRight && (
+            <RightSidebar
+              user={this.state.user}
+              members={this.state.members}
+              emotes={(this.state.data && this.state.data.emotes) || {}}
+              addEmote={this.handler.addEmote}
+              signOut={this.handler.signOut}
+              debug={this.handler.getDebug}
+            />
+          )}
+        </Box>
       </Box>
     );
   }
+}
+
+function AppToolbar({
+  currentServer,
+  currentChannel,
+  toggleLeft,
+  toggleRight,
+}: {
+  currentServer: string;
+  currentChannel: string;
+  toggleLeft: () => void;
+  toggleRight: () => void;
+}) {
+  return (
+    <AppBar position="static">
+      <Toolbar classes={{ root: "AppToolbar" }}>
+        <IconButton edge="start" onClick={toggleLeft}>
+          <Chat />
+        </IconButton>
+        <Typography variant="h6" classes={{ root: "ToolbarTitle" }}>
+          Simple-Chat
+        </Typography>
+        {currentServer && currentChannel && (
+          <Box className="ToolbarCurrent">
+            <Typography variant="h6">
+              {currentServer} - #{currentChannel}
+            </Typography>
+          </Box>
+        )}
+        <div className="ToolbarDummy" />
+        <IconButton
+          classes={{ root: "ToolbarButtonRight" }}
+          onClick={toggleRight}
+        >
+          <HorizontalSplit />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default App;
