847bb82bf52269326256d50bb407004db931eee4
ianeli1 <ianelicha@gmail.com>
2020-09-22 21:37:37 -0600

    Added onClick parameter to Emote

--- src/components/Emote.tsx
+++ src/components/Emote.tsx
@@ -1,10 +1,27 @@
 import { Tooltip } from "@material-ui/core";
 import React from "react";
 
-export function Emote({ emoteName, url }: { emoteName: string; url: string }) {
+export function Emote({
+  emoteName,
+  url,
+  onClick,
+}: {
+  emoteName: string;
+  url: string;
+  onClick?: (emoteName: string) => void;
+}) {
   return (
     <Tooltip title={emoteName} arrow placement="top">
-      <span className="emoteContainer">
+      <span
+        className="emoteContainer"
+        onClick={
+          () =>
+            onClick &&
+            onClick(
+              emoteName
+            ) /*TODO: handle this correctly with React.useCallback?*/
+        }
+      >
         <img src={url} alt={emoteName} className="emote" />
       </span>
     </Tooltip>
