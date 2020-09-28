e0b16c4c7f65f9f83bfa7b82c378ba0ca5e4cf43
ianeli1 <ianelicha@gmail.com>
2020-09-27 22:00:48 -0600

    Avatar scroller, size prop works, ASElement iface

--- src/components/AvatarScroller.tsx
+++ src/components/AvatarScroller.tsx
@@ -1,25 +1,38 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Avatar, Tooltip } from "@material-ui/core";
 import { keys } from "@material-ui/core/styles/createBreakpoints";
+import "../css/AvatarScroller.css";
+
+export type ASElement = {
+  key: string;
+  name: string;
+  icon?: string;
+};
 
 type AvatarScrollerProps = {
   size: number;
-  elements: {
-    key: string;
-    name: string;
-    icon?: string;
-  }[];
+  elements: ASElement[];
   onElementClick: (key: string) => void;
 };
 
 export function AvatarScroller(props: AvatarScrollerProps) {
+  const onElementClick = useCallback(props.onElementClick, []);
+  const style = {
+    width: props.size,
+    height: props.size,
+    fontSize: props.size / 2,
+  };
   return (
     <div className="AvatarScroller">
       {props.elements.length > 0 &&
         props.elements.map(({ key, name, icon }) => (
-          <div onClick={() => props.onElementClick(key)} key={key}>
+          <div onClick={() => onElementClick(key)} key={key} style={style}>
             <Tooltip title={name}>
-              {icon ? <Avatar src={icon} /> : <Avatar>{name[0]}</Avatar>}
+              {icon ? (
+                <Avatar src={icon} style={style} />
+              ) : (
+                <Avatar style={style}>{name[0]}</Avatar>
+              )}
             </Tooltip>
           </div>
         ))}
